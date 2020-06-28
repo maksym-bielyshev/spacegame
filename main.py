e0072cb21751fc7e9a42b5cf6a720668823d63dc
Maksym Bielyshev <maksym.bielyshev@gmail.com>
2020-06-28 13:09:32 +0300

    Set the background image.

--- main.py
+++ main.py
@@ -5,3 +5,6 @@ screen_size = [360, 600]
 
 # Create a screen to display a game.
 screen = pygame.display.set_mode(screen_size)
+
+# Set the background image.
+background = pygame.image.load('background.png')
