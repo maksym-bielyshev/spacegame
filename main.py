4d30b4fe4955bfe1f8b49354302aa83be131bd6a
Maksym Bielyshev <maksym.bielyshev@gmail.com>
2020-06-29 15:33:03 +0300

    Set the position for the background.

--- main.py
+++ main.py
@@ -8,3 +8,8 @@ screen = pygame.display.set_mode(screen_size)
 
 # Set the background image.
 background = pygame.image.load('background.png')
+
+# Set the top side of the image to align with the top of the screen.
+# And the left side of the image aligns with the left side of the screen.
+# (blit - block image transfer)
+screen.blit(background, [0, 0])
