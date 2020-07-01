6ed36fbe9522da6ed02540e7efd0155fc193f488
Maksym Bielyshev <maksym.bielyshev@gmail.com>
2020-07-01 12:54:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,14 @@ screen = pygame.display.set_mode(screen_size)
 # Set the background image.
 background = pygame.image.load('background.png')
 
-# Set the top side of the image to align with the top of the screen.
-# And the left side of the image aligns with the left side of the screen.
-# (blit - block image transfer)
-screen.blit(background, [0, 0])
+# Set the default condition for loop.
+keep_alive = True
+
+while keep_alive:
+	# Set the top side of the image to align with the top of the screen.
+    # And the left side of the image aligns with the left side of the screen.
+    # (blit - block image transfer)
+    screen.blit(background, [0, 0])
+
+	# Update the display at each iteration to change image positions.
+	pygame.display.update()
