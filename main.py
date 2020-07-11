b702d3c6a91bbcf863241b33f8a9e0d63b550a7f
Maksym Bielyshev <maksym.bielyshev@gmail.com>
2020-07-11 10:46:39 +0300

    Added planet image.

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ screen = pygame.display.set_mode(screen_size)
 # Set the background image.
 background = pygame.image.load('background.png')
 
+# Load the planet image.
+planet = pygame.image.load('p_one.png')
+
 # Set the default condition for loop.
 keep_alive = True
 
@@ -17,6 +20,9 @@ while keep_alive:
     # And the left side of the image aligns with the left side of the screen.
     # (blit - block image transfer)
     screen.blit(background, [0, 0])
+
+    # Display planet image.
+    screen.blit(planet, [140, 50])
 
     # Update the display at each iteration to change image positions.
     pygame.display.update()
