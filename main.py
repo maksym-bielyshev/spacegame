50a5dd2539803603b6697c16057b4cc638bf4cb0
Maksym Bielyshev <maksym.bielyshev@gmail.com>
2020-07-27 11:19:13 +0300

    Update the planet_x position

--- main.py
+++ main.py
@@ -34,23 +34,6 @@ while keep_alive:
     # Only the button user pressed will be True.
     keys = pygame.key.get_pressed()
 
-    # Set the top side of the image to align with the top of the screen.
-    # And the left side of the image aligns with the left side of the screen.
-    # (blit - block image transfer)
-    screen.blit(background, [0, 0])
-
-    # Display planet image.
-    screen.blit(planet, [140, 50])
-
-    # Display bullet image.
-    screen.blit(bullet, [180, 500])
-
-    # Display spaceship image.
-    screen.blit(spaceship, [160, 500])
-
-    # Update the display at each iteration to change image positions.
-    pygame.display.update()
-
     # If the value of the space key is True, the user pressed that keyboard
     # key. It would be False for every other key in the keyboard.
     if keys[pygame.K_SPACE] == True:
@@ -64,3 +47,20 @@ while keep_alive:
         planet_x = planet_x - 5
         if planet_x == 0:
             move_direction = 'right'
+
+    # Set the top side of the image to align with the top of the screen.
+    # And the left side of the image aligns with the left side of the screen.
+    # (blit - block image transfer)
+    screen.blit(background, [0, 0])
+
+    # Display planet image.
+    screen.blit(planet, [planet_x, 50])
+
+    # Display bullet image.
+    screen.blit(bullet, [180, 500])
+
+    # Display spaceship image.
+    screen.blit(spaceship, [160, 500])
+
+    # Update the display at each iteration to change image positions.
+    pygame.display.update()
